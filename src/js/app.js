8055e190dbfa4b4f30f6fad9f107b5b5a6f5591d
Kevin Solorio <soloriok@gmail.com>
2019-01-16 19:26:54 -0800

    wip: initializing web3

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,26 @@
-console.log("hello!")
+(function(window){
+  class App {
+    constructor() {
+      this.initWeb3()
+    }
+
+    async initWeb3() {
+      // modern browsers use the ethereum provider
+      if (window.ethereum) {
+        this.web3Provider = window.ethereum;
+        try {
+          await this.web3Provider.enable();
+        } catch(error) {
+          console.error("User denied account access");
+        }
+      }
+      else if (window.web3) {
+        this.web3Provider = window.web3.curentProvider;
+      }
+      else {
+        this.web3Provder = new Web3.providers.HttpProvider("http://localhost:8545");
+      }
+      window.web3 = new Web3(this.web3Provder);
+    }
+  }
+}(window)
